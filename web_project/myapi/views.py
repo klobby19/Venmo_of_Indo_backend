77fcdff401da4c7f304d7031088a50a125eb8dce
klobby19 <klobby19@gmail.com>
2020-08-14 18:50:01 -0700

    Get, put, delete API

--- web_project/myapi/views.py
+++ web_project/myapi/views.py
@@ -1,3 +1,10 @@
+from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import User
+from .serializers import UserSerializer
+
+
 from rest_framework import viewsets
 
 from .serializers import UserSerializer
@@ -9,3 +16,27 @@ class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all().order_by('name')
     serializer_class = UserSerializer
 
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail(request, pk):
+    """
+    Retrieve, update or delete a code snippet.
+    """
+    try:
+        user = User.objects.get(pk=pk)
+    except Snippet.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = UserSerializer(user)
+        return Response(user.data)
+
+    elif request.method == 'PUT':
+        serializer = UserSerializer(user, data=request.data)
+        if user.is_valid():
+            user.save()
+            return Response(user.data)
+        return Response(user.errors, status=status.HTTP_400_BAD_REQUEST)
+
+    elif request.method == 'DELETE':
+        user.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
