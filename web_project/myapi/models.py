dc91decc8301ff3b470d6dfb925d4d831141d644
klobby19 <klobby19@gmail.com>
2020-08-14 17:51:32 -0700

    Other fields (username, phonenumber, email)

--- web_project/myapi/models.py
+++ web_project/myapi/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 class User(models.Model):
     name = models.CharField(max_length=60)
+    username = models.CharField(max_length=20)
     password = models.CharField(max_length=60)
+    phonenumber = models.CharField(max_length=20)
+    email = models.CharField(max_length=60)
     def __str__(self):
         return self.name
--- web_project/myapi/serializers.py
+++ web_project/myapi/serializers.py
@@ -5,4 +5,4 @@ from .models import User
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ('id','name','password')
+        fields = ('id', 'name', 'username', 'password', 'phonenumber', 'email')
